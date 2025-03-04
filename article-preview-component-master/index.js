be707e5ab4ad67c70c5dcac05395d520bd389a96
TonyzCataldo <antoniotonyzz71@gmail.com>
2025-03-04 19:21:37 -0300

    codado

--- article-preview-component-master/index.js
+++ article-preview-component-master/index.js
@@ -2,6 +2,8 @@ function share() {
     const div = document.querySelector(".share");
     const btn = document.querySelector(".share-img");
     const svg = document.querySelector(".svg");
+    const main = document.querySelector("main");
+    main.classList.toggle("main-overflow");
     svg.classList.toggle("svg-color-change")
     div.classList.toggle("share-visible");
     btn.classList.toggle("btn-color-change");
